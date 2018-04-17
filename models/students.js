82ed0b9ab20805d4e21c3a7a1108befc69b141f7
brpthomas <pthoang6@gmail.com>
2018-04-17 13:19:07 -0400

    solution push

---
+++ models/students.js
@@ -0,0 +1,53 @@
+const db = require('../config/connection');
+
+function getAllStudents() {
+  const queryPromise = db.manyOrNone(`
+    SELECT students.id, students.fname, students.lname, students.image, students.house_id, houses.name FROM students
+    JOIN houses ON students.house_id = houses.id
+    `);
+  return queryPromise;
+}
+
+function getOneStudent(id) {
+  const queryPromise = db.one(`
+    SELECT * FROM students
+    WHERE id = $1`, id);
+  return queryPromise;
+}
+
+function createStudent(student) {
+  const query = db.one(`
+    INSERT INTO students
+    (fname, lname, image, house_id)
+    VALUES ($/fname/, $/lname/, $/image/, $/house_id/)
+    RETURNING *`,
+    student);
+  return query;
+}
+
+
+function updateStudent(student) {
+  const query = db.one(`
+    UPDATE students
+    SET fname = $/fname/, lname = $/lname/, image = $/image/,
+    house_id = $/house_id/
+    WHERE id = $/id/
+    RETURNING *`,
+    student );
+  return query;
+}
+
+function deleteStudent(id) {
+  const query = db.none(`
+    DELETE FROM students
+    WHERE id = $1`, id);
+  return query;
+}
+
+module.exports = {
+  getAllStudents,
+  getOneStudent,
+  createStudent,
+  updateStudent,
+  deleteStudent
+}
